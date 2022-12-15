a4d9993ba80650a61af74140b2b271cd8e49818d
AlexeyPanshin <dispes@mail.ru>
2022-12-15 03:28:32 +0300

    add base

---
+++ 006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 2;
+int b = 4;
+int c = 5;
+int d = 8;
+int e = 3;
+
+int max = a;
+
+if(max < a) max = a;
+if(max < b) max = b;
+if(max < c) max = c;
+if(max < d) max = d;
+if(max < e) max = e;
+
+Console.Write(max);
