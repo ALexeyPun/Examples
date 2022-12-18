1625674e125d90d9e8f1625c3e17eacde2e3b2f1
AlexeyPanshin <dispes@mail.ru>
2022-12-19 00:24:19 +0300

    add hello world and seminar one

---
+++ 009_Seminar_One/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Enter number: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter two number: ");
+int userNumberTwo = Convert.ToInt32(Console.ReadLine());
+
